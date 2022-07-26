1f56128a0f09e275a88586db69b35c3ddb898126
zack <zack1007x2@gmail.com>
2022-07-26 11:17:29 +0800

    [init]add

---
+++ Example/iPicUploader/iPicUploader.h
@@ -0,0 +1,19 @@
+//
+//  iPicUploader.h
+//  iPicUploader
+//
+//  Created by Zack on 2022/7/25.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iPicUploader.
+FOUNDATION_EXPORT double iPicUploaderVersionNumber;
+
+//! Project version string for iPicUploader.
+FOUNDATION_EXPORT const unsigned char iPicUploaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iPicUploader/PublicHeader.h>
+
+
